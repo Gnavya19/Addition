3ff556ecf1db77ff48accb23db1eb3d5fb20f9c5
Gnavya19 <navyaguddeti19@gmail.com>
2025-01-31 09:07:11 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,7 @@
+public class Add {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 4;
+        System.out.printf("Sum of %d + %d is %d", a, b, a+b);
+    }    
+}
